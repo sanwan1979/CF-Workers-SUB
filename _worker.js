d2ecf4562ce199e2b2dba08cb8754699f0c0db63
sanwan2009 <sanwan1979@gmail.com>
2025-04-26 20:13:06 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -24,9 +24,9 @@ hysteria2://a2b09545-cdc4-4286-8392-577277184e52@140.83.33.12:5556?&sni=undefine
 hysteria2://45b41158-74f9-4ad7-bba7-03e2231f86c8@140.238.11.30:4638?&sni=undefined&insecure=1#%F0%9F%87%B0%F0%9F%87%B7%20%E7%94%B2%E9%AA%A8%E6%96%87%E9%A6%96%E5%B0%94%20U
 vless://ad8d9e72-a9e2-43ee-a828-87c900f9d47a@cm.kuxueyun.com:20202?path=%2Fad8d9e72-a9e2-43ee-a828-87c900f9d47a-vless&type=ws&encryption=none&security=tls&sni=sin.952796.xyz#%F0%9F%87%B8%F0%9F%87%AC%20%E7%8B%AE%E5%9F%8E%E4%B8%AD%E8%BD%AC
 vless://53ed4107-83e4-4e54-bc22-bf93f1fcf186@cm.kuxueyun.com:20202/?type=ws&encryption=none&host=hk.952796.xyz&path=%2F53ed4107-83e4-4e54-bc22-bf93f1fcf186-vless%3Fed%3D2048&security=tls&sni=hk.952796.xyz#%F0%9F%87%AD%F0%9F%87%B0%20HKG%20%E7%A7%BB%E5%8A%A8V6
-vless://8fb32f48-0051-401f-a1bc-48406ad5aeb2@45.32.164.48:37345?type=tcp&security=reality&sni=addons.mozilla.org&pbk=qhTzYYIgBzDLNYR79oxftqdo1kzL-1_hGJKfqrOliCY&flow=xtls-rprx-vision&fp=chrome#X|Vicky|400GB
-vless://4415fc08-9d11-4a22-a38e-2468644f4e74@132.226.132.226:25608?type=tcp&security=reality&sni=www.speedtest.org&pbk=UtL7E0Gmxj3X5JdcPAutpTRKo7K2hugkR0vwk2XroUM&flow=xtls-rprx-vision&fp=chrome#L|linuxdo|400GB
-vless://9326d2fc-5d5e-31e4-8e7b-b3800ae6c642@154.201.91.70:443?flow=xtls-rprx-vision&encryption=none&type=tcp&security=reality&sni=learn.microsoft.com&fp=random&pbk=6CRKsmWYwlivNa1Zv_X_wOVZHdVujOnbP5eGJdUhq3E&sid=51e5cd9ae8199dd1&spx=&#9929+CMI2节点`
+ss://YWVzLTEyOC1nY206VmFjSXJpRWJ6N3VUVVdOb1d5SFR6aTlOMHFlVkhRZlExa2h4UGNHdGdITT0@1.168.185.56:23333#%F0%9F%87%B9%F0%9F%87%BC%20%E5%8F%B0%E6%B9%BE%E5%AE%B6%E5%AE%BDIP
+vless://9326d2fc-5d5e-31e4-8e7b-b3800ae6c642@154.201.91.70:443?flow=xtls-rprx-vision&encryption=none&type=tcp&security=reality&sni=learn.microsoft.com&fp=random&pbk=6CRKsmWYwlivNa1Zv_X_wOVZHdVujOnbP5eGJdUhq3E&sid=51e5cd9ae8199dd1&spx=&#9929+CMI2节点
+https://aw.abc8848.com/api/v1/client/subscribe?token=381569c5073cf7333174f7599e78b2c9`
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
