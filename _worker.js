b767bd0c47d188e5ed05359ac85631f1b236f19c
sanwan2009 <sanwan1979@gmail.com>
2025-07-31 12:38:41 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -26,7 +26,8 @@ vless://ad8d9e72-a9e2-43ee-a828-87c900f9d47a@ddns.miskak.net:1234?path=%2Fad8d9e
 vless://eed9d023-a9be-4740-9045-7c8b7ea8be0c@ddns.miskak.net:1234/?type=ws&encryption=none&host=hk.952796.xyz&path=%2Feed9d023-a9be-4740-9045-7c8b7ea8be0c-vless%3Fed%3D2048&security=tls&sni=hk.952796.xyz#%F0%9F%87%AD%F0%9F%87%B0%20HKG%20%E7%A7%BB%E5%8A%A8V6
 vless://ffffffff-ffff-ffff-ffff-ffffffffffff@ws.952796.xyz:443?path=%2F&security=tls&encryption=none&host=ws.952796.xyz&type=ws&sni=ws.952796.xyz#%F0%9F%87%B8%F0%9F%87%AC%20%E6%96%B0%E5%8A%A0%E5%9D%A1%E8%87%AA%E5%BB%BA
 vless://ffffffff-ffff-ffff-ffff-ffffffffffff@ddns.miskak.net:1234?path=%2F&security=tls&encryption=none&host=ws.clou.eu.org&type=ws&sni=ws.clou.eu.org#%F0%9F%87%B0%F0%9F%87%B7%20%E9%9F%A9%E5%9B%BD
-vless://ffffffff-ffff-ffff-ffff-ffffffffffff@nat64.952796.xyz:443?path=%2F&security=tls&encryption=none&host=nat64.952796.xyz&type=ws&sni=nat64.952796.xyz#%F0%9F%87%AD%F0%9F%87%B0%20NAT64`
+vless://ffffffff-ffff-ffff-ffff-ffffffffffff@nat64.952796.xyz:443?path=%2F&security=tls&encryption=none&host=nat64.952796.xyz&type=ws&sni=nat64.952796.xyz#%F0%9F%87%AD%F0%9F%87%B0%20NAT64
+https://cfxr.eu.org/autoconvert`
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
