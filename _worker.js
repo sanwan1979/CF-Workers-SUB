cc29fccafcc39b9b679d0bc476e5302f545bb4a7
sanwan2009 <sanwan1979@gmail.com>
2025-08-19 20:29:04 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -16,7 +16,7 @@ let MainData = `
 hy2://0d2e03da-9226-47b4-b2e8-ed42129479ab@146.56.134.229:3001/?insecure=1#%F0%9F%87%B0%F0%9F%87%B7%20%E6%98%9F%E4%BA%91
 vless://0d2e03da-9226-47b4-b2e8-ed42129479ab@146.56.134.229:1234/?type=ws&encryption=none&host=miskak.eu.org&path=%2F0d2e03da-9226-47b4-b2e8-ed42129479ab-vless%3Fed%3D2048&security=tls&sni=miskak.eu.org&alpn=default#%F0%9F%87%B0%F0%9F%87%B7%20WART%20%E9%A6%96%E5%B0%94
 vless://ffffffff-ffff-ffff-ffff-ffffffffffff@198.12.95.225:443/?type=ws&encryption=none&host=kom.clou.eu.org&path=%2F%3Fed%3D2048&security=tls&sni=kom.clou.eu.org#%F0%9F%87%AD%F0%9F%87%B0%20WART
-vless://ad8d9e72-a9e2-43ee-a828-87c900f9d47a@111.119.212.208:59876/?type=tcp&encryption=none&sni=addons.mozilla.org&fp=chrome&security=reality&pbk=RTlycYCi0XA3jCVkhMjJ6drAO3XcXEJzhVzcHkNNjAI&sid=#%F0%9F%87%B8%F0%9F%87%AC%20%E7%8B%AE%E5%9F%8E
+hysteria2://e576c077-b653-4ee5-9924-311e708a5ee0@sin.952796.xyz:30574?peer=sin.952796.xyz&insecure=0&sni=sin.952796.xyz&alpn=h3#e576c077-singbox_hysteria2
 hy2://a323e0ac-145b-4f1d-aef3-99950447a960@192.9.129.216:55556/?insecure=1&sni=www.bing.com#%F0%9F%87%BA%F0%9F%87%B2%20%E7%94%B2%E9%AA%A8%E6%96%87%20%E5%9C%A3%E4%BD%95%E5%A1%9E
 vless://8702cb64-0132-476d-871e-e41c62e7efbd@146.56.134.229:1234/?type=ws&encryption=none&host=pierce-scratch-wider-won.trycloudflare.com&path=%2Fsba-vl%3Fed%3D2048&security=tls&sni=pierce-scratch-wider-won.trycloudflare.com#%F0%9F%87%BA%F0%9F%87%B8%20%E5%9C%A3%E4%BD%95%E5%A1%9E
 vless://096382d2-d25b-4bcb-87b4-299e6dcfa2e7@146.56.134.229:1234/?type=ws&encryption=none&flow=&host=screenshot-segments-metro-warren.trycloudflare.com&path=%2Fsba-vl%3Fed%3D2048&security=tls&sni=screenshot-segments-metro-warren.trycloudflare.com#%F0%9F%87%BA%F0%9F%87%B8%20%E5%9C%A3%E8%8D%B7%E8%A5%BF
