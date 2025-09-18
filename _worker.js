87216e1af43fe43ef931bc1729174cbce40bef57
sanwan2009 <sanwan1979@gmail.com>
2025-09-18 23:35:17 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -29,7 +29,8 @@ vless://8483edc8-03f6-4c7d-a77e-cc73558a2064@47.245.32.127:52147/?type=tcp&encry
 vless://a3101a99-736c-4986-85a1-db2d58448bf0@140.83.33.12:43697/?type=tcp&encryption=none&flow=&sni=yahoo.com&fp=chrome&security=reality&pbk=qati7KkL0kH5XWjIm_D2WKyScs2HJZ0rZDMwxkCoEnE&sid=200e1e0e8d#%F0%9F%87%AF%F0%9F%87%B5%20%E7%94%B2%E9%AA%A8%E6%96%87%20%E4%B8%9C%E4%BA%ACU2
 vless://0318b3b9-8fe1-45bd-b10d-45dd343bed5b@120.79.252.15:47898/?type=tcp&encryption=none&flow=&sni=f.pan.wo.cn&fp=chrome&security=reality&pbk=PW9Ca5riQpiFQNl4nm3CwbfFnsPcp-ifO_lg5e2-V3M&sid=cd79a8568cd9#%F0%9F%87%A8%F0%9F%87%B3%20%E6%B7%B1%E5%9C%B3-
 vless://6c9df391-7688-43b0-b722-865c6b2c02dd@47.245.32.127:58061?security=reality&encryption=none&pbk=lKzaecQK5fQDlAipNvEQfrE2COg_3-EHIWyycvh2qSU&headerType=none&fp=chrome&spx=%2F&type=tcp&sni=yahoo.com&sid=1cf12bfdfa#%F0%9F%87%AF%F0%9F%87%B5%20%E9%98%BF%E9%87%8C%E4%BA%91%20%E4%B8%9C%E4%BA%AC
-vless://f742cb63-b890-3015-a2a0-74dbf8fe6e73@111.119.212.208:26453/?type=tcp&encryption=none&flow=xtls-rprx-vision&sni=learn.microsoft.com&fp=random&security=reality&pbk=IdKNuC13pevhwfd3uxbN2UBt_b2Fol6KJv2p8KhrXz4&sid=a14b32c72547982d#%F0%9F%87%B2%F0%9F%87%BE%20%E9%A6%AC%E4%BE%86%E8%A5%BF%E4%BA%9E`
+vless://f742cb63-b890-3015-a2a0-74dbf8fe6e73@111.119.212.208:26453/?type=tcp&encryption=none&flow=xtls-rprx-vision&sni=learn.microsoft.com&fp=random&security=reality&pbk=IdKNuC13pevhwfd3uxbN2UBt_b2Fol6KJv2p8KhrXz4&sid=a14b32c72547982d#%F0%9F%87%B2%F0%9F%87%BE%20%E9%A6%AC%E4%BE%86%E8%A5%BF%E4%BA%9E
+`
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
